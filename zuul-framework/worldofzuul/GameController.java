217b1cdb9f0f31774093a2a711482357c34b3e34
simon <sival19@student.sdu.dk>
2020-10-27 11:55:38 +0100

    added Gamecontroller and Player

--- zuul-framework/worldofzuul/GameController.java
+++ zuul-framework/worldofzuul/GameController.java
@@ -1,4 +1,21 @@
 package worldofzuul;
 
 public class GameController {
+
+    //attributes for game controller
+    private Player;
+    private Room;
+
+
+
+    //methods
+    public void addPlayer() {
+
+    }
+
+    public void addRoom() {
+
+    }
+
+
 }
