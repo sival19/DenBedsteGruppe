d025ff10c132f07aed0993689a9a8aa0a7ce02f2
Nicolaj <Iversen2903@hotmail.com>
2020-10-27 13:04:34 +0100

    Added characters: Wizard, PoorMan and PoorMan

--- zuul-framework/worldofzuul/Characters.java
+++ zuul-framework/worldofzuul/Characters.java
@@ -1,57 +1,26 @@
 package worldofzuul;
-import javax.naming.Name;
+
 import java.util.ArrayList;
 
 public class Characters extends InteractiveObjects {
 
-    //Note: Overide metoder fra InteractiveObjects - (metoder: 'getName' og 'getInfo')
+    //Created a Wizard
+    public void Wizard() {
+        new InteractiveObjects("Wizard", "Welcome") //info: add playername
+    }
+
+    //Created a poor man
+    public void PoorMan() {
+            new InteractiveObjects("S.U. modtager", "I'm poor."); //edit info
+        }
+
+    //Created a rich man
+    public void RichMan() {
+        new InteractiveObjects("Joakim von And", "I'm rich.");
+    }
+
 
     int Tasks[];
     String InteractWith;
-
-  //  Constructor - creates characters
-  //  Characters() {
- //   }
-
-//    //Provides a name
-//     void Name(String newName) {
-//
-//        //Refers to an overrided method (not yet done)
-//        name = newName;
-//    }
-//
-//    //Provides info - 'chat box'
-//     void Info(String[] newInfo) {
-//
-//        //Refers to an overrided method (not yet done)
-//        name = newInfo;
-//    }
-
-    private void createCharacter();
-    //Creates a new character - Wizard
-
-    //ALl the characters
-    Characters Wizard, PoorMan, RichMan;
-
-    //Creating the new characters
-    Wizard = new Characters("Welcome " + playerName + "!");
-    PoorMan = new PoorMan("I'm poor.");
-    RichMan = new Richman("I'm rich.");
-
-
-    //Afventer gruppe's getName metode
-    Wizard.setName("Wizard")
-
-    Wizard.setInfo("Filler - chat box")
-    //Next interaction.
-    //Benytter gruppes 1's metode (getInfo) til at tilføje flere interaction for hver character
-
-    }
-
-    public static void main(String[] args) {
-
-    static void firstInfo();
-
-        }
 }
-}
+
--- zuul-framework/worldofzuul/GameController.java
+++ zuul-framework/worldofzuul/GameController.java
@@ -19,6 +19,7 @@ public class GameController {
 
     public void addRoom() {
 
+
     }
 
 
