9d2cc41c6bd838a8b06b34346d650f73d5d92873
simon <sival19@student.sdu.dk>
2020-11-25 21:09:41 +0100

    Man kan nu bevæge mellem alle rum næsten fejlfrit xD

--- zuul-framework/src/sample/LibraryController.java
+++ zuul-framework/src/sample/LibraryController.java
@@ -1,6 +1,7 @@
 package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import worldofzuul.*;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -8,12 +9,33 @@ import javafx.scene.control.Button;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 
 
-public class LibraryController {
+public class LibraryController extends StationController {
 
 
-    @FXML private Button button;
+    @FXML private Button school_lib, richroom_lib;
+
+    @FXML
+    public void goSchool() throws IOException{
+        getTest().processCommand(CommandWord.GO, "skolen");
+        Parent loader = FXMLLoader.load(getClass().getResource("school.fxml"));
+        Stage stage = (Stage) school_lib.getScene().getWindow();
+
+        stage.setScene(new Scene(loader, 731, 439));
+    }
+
+    @FXML
+    public void goRichroom() throws IOException {
+
+        getTest().processCommand(CommandWord.GO, "rigmandsgården");
+        Parent loader = FXMLLoader.load(getClass().getResource("richroom.fxml"));
+        Stage stage = (Stage) richroom_lib.getScene().getWindow();
+
+        stage.setScene(new Scene(loader, 731, 439));
+
+    }
 
 
     }
--- zuul-framework/src/sample/PoorroomController.java
+++ zuul-framework/src/sample/PoorroomController.java
@@ -2,6 +2,9 @@ package sample;
 
 import javafx.fxml.FXML;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.ClipboardContent;
@@ -9,11 +12,36 @@ import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 import worldofzuul.Character;
+import worldofzuul.CommandWord;
 
-public class PoorroomController {
+import java.io.IOException;
 
-    @FXML private Button poorChild;
+public class PoorroomController extends StationController {
+
+    @FXML private Button poorChild, school_poor, station_poor;
+    @FXML
+    public void goStation() throws IOException {
+        getTest().processCommand(CommandWord.GO, "stationen");
+        Parent loader = FXMLLoader.load(getClass().getResource("stationSecond.fxml"));
+        Stage stage = (Stage) station_poor.getScene().getWindow();
+
+        stage.setScene(new Scene(loader, 731, 439));
+
+    }
+
+    @FXML
+    public void goSchool() throws IOException {
+
+        //use command go and go to skolen
+        getTest().processCommand(CommandWord.GO, "skolen");
+        Parent loader = FXMLLoader.load(getClass().getResource("school.fxml"));
+        Stage stage = (Stage) school_poor.getScene().getWindow();
+
+        stage.setScene(new Scene(loader, 731, 439));
+
+    }
 
     @FXML private Circle apple;
 
