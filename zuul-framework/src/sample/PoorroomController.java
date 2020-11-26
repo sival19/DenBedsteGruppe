a47035fec085faf4967a75edcdd1c971ca93b6a2
Anne Cathrine Kirkegaard <kirkegaard.annecathrine@gmail.com>
2020-11-26 10:52:47 +0100

    Auto stash before merge of "NN" and "origin/NN"

--- zuul-framework/src/sample/PoorroomController.java
+++ zuul-framework/src/sample/PoorroomController.java
@@ -4,27 +4,24 @@ import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
+import javafx.scene.input.*;
 import javafx.scene.shape.Circle;
 import worldofzuul.Character;
 
 public class PoorroomController {
 
-    @FXML private Button poorChild;
+    @FXML private Button dreng;
 
     @FXML private Circle apple;
 
     @FXML
     private TextField text;
     public void talkPoorChild() {
-        poorChild.getId();
+        dreng.getId();
         // Somehow call getCharMessage from Game class with button id as argument. Make sure char name == buttonid
         text.setText("Jeg er sulten"); // set text to getCharMessage return value
 
-        // 
+        //
     }
 
     // TEST
@@ -43,5 +40,22 @@ public class PoorroomController {
         db.setContent(content);
 
         mouseEvent.consume();
+    }
+
+    public void handleDragDropped(DragEvent dragEvent) {
+        /* data dropped */
+        /* if there is a string data on dragboard, read it and use it */
+        Dragboard db = dragEvent.getDragboard();
+        boolean success = false;
+        if (db.hasString()) {
+            text.setText(db.getString());
+            success = true;
+        }
+        /* let the source know whether the string was successfully
+         * transferred and used */
+        dragEvent.setDropCompleted(success);
+
+        dragEvent.consume();
+
     }
 }
