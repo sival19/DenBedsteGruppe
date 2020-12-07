59111a9e48d31303398ac9c13d2ca2d53d292478
Nadia <nadianiemier@hotmail.com>
2020-12-07 12:55:47 +0100

    Ældre dame på biblo

--- zuul-framework/src/sample/LibraryController.java
+++ zuul-framework/src/sample/LibraryController.java
@@ -13,6 +13,11 @@ public class LibraryController extends Main implements Initializable {
     @FXML
     private Label text;
 
+    //CHARACTER INTERACTION
+    public void talkOldLady() {
+        text.setText("Jeg har strukket alt for mange halstørklæder, kender du nogen som kan bruge dem?");
+    }
+
     // INVENTORY MANAGEMENT
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
